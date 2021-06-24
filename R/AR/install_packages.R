6ac05a4125ddf65d20eaddbb8da2218c7e9ccadf
JensDeCeukeleire <jens.de-ceukeleire@agilent.com>
2021-06-24 09:04:43 +0200

    fix typo install_packages.R

--- R/AR/install_packages.R
+++ R/AR/install_packages.R
@@ -3,7 +3,7 @@ install.packages("BiocManager", repos="https://cloud.r-project.org")
 BiocManager::install(c("Biostrings", "IRanges", "Rsamtools", "GenomicRanges", "GenomicAlignments"))
 
 # Install remaining (regular) packages
-install.packages("optparse", repos="https://cloud.r-project.org"i)
+install.packages("optparse", repos="https://cloud.r-project.org")
 install.packages("dplyr", repos="https://cloud.r-project.org")
 install.packages("devtools", repos="https://cloud.r-project.org")
 suppressPackageStartupMessages(library("devtools"))
