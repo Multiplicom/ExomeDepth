04a59e097904796b59b3821494791678965c133a
JensDeCeukeleire <jens.de-ceukeleire@agilent.com>
2021-06-24 09:01:12 +0200

    Update install_packages.R with BiocManager and add mirror for all

--- R/AR/install_packages.R
+++ R/AR/install_packages.R
@@ -1,5 +1,10 @@
-install.packages("optparse")
-install.packages("dplyr")
-install.packages("devtools")
+# Install BiocManager and required packages for ExomeDepth
+install.packages("BiocManager", repos="https://cloud.r-project.org")
+BiocManager::install(c("Biostrings", "IRanges", "Rsamtools", "GenomicRanges", "GenomicAlignments"))
+
+# Install remaining (regular) packages
+install.packages("optparse", repos="https://cloud.r-project.org"i)
+install.packages("dplyr", repos="https://cloud.r-project.org")
+install.packages("devtools", repos="https://cloud.r-project.org")
 suppressPackageStartupMessages(library("devtools"))
 devtools::install_github("Multiplicom/ExomeDepth", force = TRUE)
